5780bba787b4543ec69817db7f918bd030765aba
stanislas <stan.smithley@gmail.com>
2022-11-16 21:51:53 +0100

    mise en place de condition pour choix

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const readline = require('readline')
+const {stdin: input, stdout: output} = require('process')
+const { pipeline } = require('stream')
+const rl = readline.createInterface(input, output)
+
+let i =0
+
+
 function Pizza (id, nom, prix){
     this.id = id
     this.nom = nom
@@ -13,4 +21,22 @@ pizza.push(new Pizza ("3","Authentique raclette", "12"))
 pizza.push(new Pizza ("4", "Cheesy & veggi","13"))
 
 console.log(pizza)
+
+function Pannier (nom, prenom, namePizza, qte){
+    this.nom = nom
+    this.prenom = prenom
+    this.namePizza = namePizza
+    this.qte = qte
+}
+
+
+rl.question('choisissez votre pizza  : \n', choix=>{
+    while(choix != pizza[i].id){
+        i++
+    }
+     
+    console.log(pizza[i].nom)
+   
+
+})
 
