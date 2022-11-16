77440b81f5d86be9b0016d51148fa1956232a0be
stanislas <stan.smithley@gmail.com>
2022-11-16 21:32:48 +0100

    mise en place d'un tableau

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+function Pizza (id, nom, prix){
+    this.id = id
+    this.nom = nom
+    this.prix = prix
+}
+
+
+let pizza = []
+
+pizza.push(new Pizza ("1", "3 fromages","12"))
+pizza.push(new Pizza ("2", "4 Miel","8"))
+pizza.push(new Pizza ("3","Authentique raclette", "12"))
+pizza.push(new Pizza ("4", "Cheesy & veggi","13"))
+
+console.log(pizza)
 
